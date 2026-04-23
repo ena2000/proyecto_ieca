485dbcb3fea238b4ce65ceeab278e8dc4f722ace
Milena Mariscal <iammilenamariscal@outlook.com>
2026-04-23 02:20:18 -0500

    Se implemento pantalla de Ingresos

--- src/app/pages/ingresos/ingresos.component.ts
+++ src/app/pages/ingresos/ingresos.component.ts
@@ -1,17 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { addIcons } from 'ionicons';
+import { 
+  calendarOutline, 
+  cashOutline, 
+  documentTextOutline, 
+  cloudUploadOutline, 
+  saveOutline,
+  notificationsOutline // Icono extra para el toolbar
+} from 'ionicons/icons';
 
 @Component({
   selector: 'app-ingresos',
   templateUrl: './ingresos.component.html',
   styleUrls: ['./ingresos.component.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent]
+  imports: [IonicModule, CommonModule]
 })
-export class IngresosComponent  implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {}
-
+export class IngresosComponent {
+  constructor() {
+    addIcons({ 
+      'calendar-outline': calendarOutline,
+      'cash-outline': cashOutline,
+      'document-text-outline': documentTextOutline,
+      'cloud-upload-outline': cloudUploadOutline,
+      'save-outline': saveOutline,
+      'notifications-outline': notificationsOutline
+    });
+  }
 }
