90b7afbcea2fa8394ca116713fa57d449d3787fa
Milena Mariscal <iammilenamariscal@outlook.com>
2026-05-03 20:37:05 -0500

    Creacion de modal para edicion

---
+++ src/app/components/modal-form/modal-form.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonModal, IonButton, IonInput, IonItem, IonLabel } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-modal-form',
+  standalone: true,
+  templateUrl: './modal-form.component.html',
+  styleUrls: ['./modal-form.component.scss'],
+  imports: [CommonModule, FormsModule, IonModal, IonButton, IonInput, IonItem, IonLabel]
+})
+export class ModalFormComponent {
+
+  @Input() isOpen: boolean = false;
+  @Input() titulo: string = 'Formulario';
+  @Input() fields: any[] = []; // 👈 dinámico
+  @Input() data: any = {};     // 👈 datos a editar
+
+  @Output() close = new EventEmitter<void>();
+  @Output() save = new EventEmitter<any>();
+
+  cerrar() {
+    this.close.emit();
+  }
+
+  guardar() {
+    this.save.emit(this.data);
+  }
+}
